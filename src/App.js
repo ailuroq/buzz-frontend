5652f84e394cb33c4553cd761b9d5781e950651b
ailuroq <yaroslav_ovsyannikov@list.ru>
2020-12-26 05:33:05 +0300

    added router and profile

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
-import Messenger from "./components/content/Messenger/Messenger";
+import Messenger from "./components/Content/Messenger/Messenger";
 import styles from './App.module.css'
+import {Route} from "react-router-dom";
+import Profile from "./components/Content/Profile/Profile";
 
 const App = (props) => {
     return (
-        <div className={styles.mainWrapper}>
-            <Header />
-            <Nav/>
-            <Messenger/>
-        </div>
+            <div className={styles.mainWrapper}>
+                <Header />
+                <Nav/>
+                <Route exact path='/messenger' component={Messenger}/>
+                <Route exact path='/profile' component={Profile}/>
+
+            </div>
     )
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import styles from './Header.module.css'
-import {Container, Row, Col} from 'bootstrap-4-react'
+import {Container, Row, Col, Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import logo from '../../static/straw.png'
 import {ErrorMessage, Field, Form, Formik} from 'formik'
+
 const Header = (props) => {
     return (
         <div className={styles.header}>
             <Container>
-                <Row justifyContent="between">
-                    <Col col="sm">
-                        <img className={styles.logotype} src={logo} alt=""/>
+                <Row>
+                    <Col sm={10}>
+                        <img className={styles.logotype} src={logo}/>
                     </Col>
-                    <Col col="sm">
-                        <a href="/profile">My profile</a>
+                    <Col sm={2}>
+                        <div className={styles.profile}>
+                            <a href="/profile">My profile</a>
+                        </div>
                     </Col>
                 </Row>
             </Container>
