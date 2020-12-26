2403d73652863daf00a0e7769652b723d4f4e391
ailuroq <yaroslav_ovsyannikov@list.ru>
2020-12-26 06:11:36 +0300

    added redux devtools

--- src/components/Content/Profile/Profile.js
+++ src/components/Content/Profile/Profile.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import {Col, Container, Row} from "react-bootstrap";
+import {connect} from "react-redux";
 
-const Profile = (props) => {
+const Profile = props => {
     return (
         <div>
             <div>
-
+                <Container>
+                    <Row>
+                        <Col sm={4}>
+                            Photo
+                        </Col>
+                        <Col sm={8}>
+                            <p>props.name</p>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </div>
     )
 }
 
-export default Profile
+const mapStateToProps = (state) => {
+    return {
+        name: state.name,
+        status: state.status
+    }
+}
+
+export default connect(mapStateToProps)(Profile)
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,6 +3,6 @@ import {combineReducers, createStore} from "redux";
 
 const store = createStore(combineReducers({
     mainReducer
-}))
+}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
